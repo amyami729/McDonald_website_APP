50f934e9231b994ee0381db4fe648a34f6b59dfd
amyami729 <medical178442@gmail.com>
2021-09-28 05:24:17 +0800

    init

--- src/components/goodsSpecification/GoodsSpecification.vue
+++ src/components/goodsSpecification/GoodsSpecification.vue
@@ -55,7 +55,7 @@
                             </div>
                         </li>
                     </ul>
-            </div>
+                </div>
             </div>
         </div>
     </transition>
